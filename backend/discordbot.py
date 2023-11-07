5d9fe6f32e0c1c6d2c70ff21595bad75fbdf875a
daiki <kishita167@icloud.com>
2023-11-07 23:18:18 +0900

    discordbot

--- backend/discordbot.py
+++ backend/discordbot.py
@@ -3,14 +3,14 @@ import discord
 from discord import app_commands
 
 #DB操作
-from backend.sql.ORM import GetUserFromColor 
+from sql.ORM import GetUserFromColor 
 
 #環境ファイル
 import os
 from dotenv import load_dotenv
 
 #モジュール
-from backend.modules.modules import Get_datas
+from modules.modules import Get_datas
 
 load_dotenv("backend/sql/envs/.envs")
 
@@ -29,17 +29,17 @@ async def on_ready():
     await bot.sync()
 
 @bot.command(name="show",description="研究室に誰がいるかな")
-async def show(interaction:discord.Integration):
+async def show(interaction):
     post_word=""
     ctx=interaction
     users=Get_datas(GetUserFromColor("green"))
     if len(users)==0:
         post_word="現在研究室には誰もいません\nこれは富樫研の危機か？"
-        await ctx.responce.send_message(post_word)
+        await ctx.response.send_message(post_word)
     else:
         post_word="↓今現在研究室にいる人↓\n"
         for user in users:
-            post_word+=f"・{user.name}\n"
-        await ctx.responce.send_message(post_word)
+            post_word+=f"・{user['name']}\n"
+        await ctx.response.send_message(post_word)
 
 client.run(TOKEN)
